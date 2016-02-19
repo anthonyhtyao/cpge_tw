d3b19e4b4d9976c7fa4e8241089eccf1820445ee
HUANG <yhuang@enseirb-matmeca.fr>
2016-02-20 00:03:08 +0100

    Add reply modal and JS

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,8 +1,14 @@
-$(document).ready( function() {
+// $(document).ready( function() {
 
-	$(".reply-comment").click( function() {
-		$("#reply-dialog").dialog("open");
-	});
-	$("#reply-dialog").dialog({autoOpen : false, modal : true, show : "blind", hide : "blind"});  
+// 	$(".reply-comment").click( function() {
+// 		$("#reply-dialog").dialog("open");
+// 	});
+// 	$("#reply-dialog").dialog({autoOpen : false, modal : true, show : "blind", hide : "blind"});  
+// });
+
+$(document).on("click", ".open-reply", function () {
+     var myBookId = $(this).attr("data-id");
+     var author = $(this).attr("comment-author")
+     $("#modal-pid").html(myBookId);
+     $("#modal-media-heading").html(author)
 });
-
