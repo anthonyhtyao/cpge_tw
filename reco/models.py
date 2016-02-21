62292ab1a7d5492961b73a7ed187605e3ac950a5
HUANG <yhuang@enseirb-matmeca.fr>
2016-02-21 11:14:32 +0100

    add name variable in comment model

--- reco/models.py
+++ reco/models.py
@@ -17,15 +17,23 @@ class UserProfile(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(UserProfile, null=True)
+    author = models.ForeignKey(UserProfile, null=True, blank=True)
     content = models.TextField(null=True)
     date = models.DateTimeField(auto_now_add=True, auto_now=False)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     parent = GenericForeignKey('content_type', 'object_id')
+    name = models.CharField(max_length = 128, blank = True, null = True)
 
     def __str__(self):
         return self.content 
+
+    def save(self, *args, **kwargs):
+        if self.author:
+            self.name = self.author.name
+        if not(self.author or self.name):
+            self.name = 'unknown'
+        super(Comment, self).save(*args, **kwargs)
 
 class Article(models.Model):
     title = models.CharField(max_length=128)
