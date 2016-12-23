74872c838113d60c2b22227dc9f93d44c2b0fc0c
htyao <anthonyhtyao@gmail.com>
2016-12-23 18:40:25 +0100

    Move database to postgresql

--- cpge_tw/settings.py
+++ cpge_tw/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'cpge_tw.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cpge',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
