7b2e524c3916878f41067f642b50a5d9ab36b948
htyao <anthonyhtyao@gmail.com>
2016-12-25 22:57:39 +0100

    Modify article's page

--- cpge_tw/views.py
+++ cpge_tw/views.py
@@ -18,17 +18,14 @@ def index(request, loginMsg=""):
     return render(request, 'cpge_tw/index.html', context_dict)
 
 def article(request,articleID):
-    article_dict = {}
+    returnForm = {}
     try:
         article = Article.objects.get(id=articleID)
-        article_dict['article'] = article
-        articleType = ContentType.objects.get_for_model(article)
-        comments = Comment.objects.filter(content_type = articleType.id, object_id = articleID).order_by('-date')
-        article_dict['comments'] = comments
-
+        returnForm['title'] = article.title
+        returnForm['content'] = article.contentHtml
     except Article.DoesNotExist:
-        pass
-    return render(request, 'cpge_tw/article.html', article_dict)
+        HttpResponseRedirect('/')
+    return render(request, 'cpge_tw/article.html', returnForm)
 
 def articlecomment(request, articleID):
     if request.method == 'POST':
